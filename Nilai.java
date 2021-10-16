1a1962a4d4d08fc686a65f71b4cb5a806cdf4a89
tirza <tirzaunggu@gmail.com>
2021-10-16 22:34:37 +0900

    first commit

---
+++ Nilai.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Nilai {
+    public static void main(String[] args) {
+        int nilai;
+        Scanner data = new Scanner(System.in);
+
+        System.out.println("-----------------------");
+        System.out.println("|--- Rentang Nilai ---|" );
+        System.out.println("-----------------------");
+        System.out.println("|     A" + " : " +  "81 - 100    |" + "\n" +
+                           "|     B" + " : " +  "61 - 80     |" + "\n" +
+                           "|     C" + " : " +  "41 - 60     |"   + "\n" +
+                           "|     D" + " : " +  "21 - 40     |" + "\n" +
+                           "|     E" + " : " +  "0 - 20      |");
+        System.out.println("-----------------------" + "\n");
+
+        System.out.println("Silahkan masukkan nilai :  ");
+        nilai = data.nextInt();
+        
+        //function
+        if(nilai >= 0 && nilai <= 20){
+            System.out.println("Nilai Kamu adalah E");
+        }else if(nilai >= 21 && nilai <= 40){
+            System.out.println("Nilai Kamu adalah D");
+        }else if (nilai >= 41 && nilai <= 60){
+            System.out.println("Nilai Kamu adalah C");
+        }else if (nilai >= 61 && nilai <= 80){
+            System.out.println("Nilai Kamu adalah B");
+        }else if (nilai >= 81 && nilai <= 100){
+            System.out.println("Nilai Kamu adalah A");
+        }else if (nilai >=100){
+            System.out.println("Nilai yang kamu masukkan tidak sesuai");
+        }
+    }
+}
